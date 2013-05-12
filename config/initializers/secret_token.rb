ae9ab1580bec5e4fee29931c38a068b970c29c6f
Cassio Cabral <cassioscabral@gmail.com>
2013-05-11 22:11:38 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Integratechcaroneiro::Application.config.secret_token = '9709b1096e431599e6d3a590f28fb9d926d38956f085baaa6dbd1e4560832e9ac5d3a8a98154f62d71cc8d466cc825e99cd6158c1eead6b1a2090359fb5541ab'
